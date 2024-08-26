7b04f44214c0ac527622ef88df5305deb47c69e6
devtvas <tarcisio.word@gmail.com>
2024-08-26 16:10:53 -0500

    nextjs - update tela login and register

--- pages/cadastro.js
+++ pages/cadastro.js
@@ -1,13 +1,21 @@
 import styles from "../styles/Login.module.css"
 
+import Button from '../src/components/button/button'
+import Input from '../src/components/input/input'
 import LoginCard from "../src/components/loginCard/loginCard"
+
 
 export default function CadastroPage() {
     return (
         <div className={styles.background}>
-        <LoginCard title="Crie sua conta"> 
-                Cadastro
-                </LoginCard>
+            <LoginCard title="Crie sua conta">
+                <form className={styles.form}>
+                    <Input title="text" placeholder="Digite seu nome"></Input>
+                    <Input title="email" placeholder="Digite seu e-mail"></Input>
+                    <Input title="senha" placeholder="Digite sua senha"></Input>
+                    <Button>Cadastrar</Button>
+                </form>
+            </LoginCard>
         </div>
     )
 }
--- pages/login.js
+++ pages/login.js
@@ -1,11 +1,18 @@
-import LoginCard from "../src/components/loginCard/loginCard"
 import styles from "../styles/Login.module.css"
+
+import Button from '../src/components/button/button'
+import Input from '../src/components/input/input'
+import LoginCard from "../src/components/loginCard/loginCard"
 
 export default function LoginPage() {
     return (
         <div className={styles.background}>
-            <LoginCard > 
-                
+            <LoginCard title="Entre em sua conta">
+              <form className={styles.form}>
+              <Input title="email" placeholder="Digite seu e-mail"></Input>
+                <Input title="senha" placeholder="Digite sua senha"></Input>
+                <Button>Entrar</Button>
+              </form>
             </LoginCard>
         </div>
     )
